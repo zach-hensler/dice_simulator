c290c3b3eefa3505295b735cd93ad65e0e4a12fd
Zach H <zhensler12@gmail.com>
2024-03-02 00:34:59 -0500

    Update vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: 'https://zach-hensler.github.io/gallery',
+  base: 'https://zach-hensler.github.io/dice_simulator',
   root: './',
   build: {
     outDir: 'build',
